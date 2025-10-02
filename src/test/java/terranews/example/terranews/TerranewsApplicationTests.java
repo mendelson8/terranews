01d23597f17b8aa295b571d5be596a9df3bd05ae
mendelson8 <kuba.mendel@wp.pl>
2025-10-02 22:48:47 +0200

    2

---
+++ src/test/java/terranews/example/terranews/TerranewsApplicationTests.java
@@ -0,0 +1,13 @@
+package terranews.example.terranews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerranewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

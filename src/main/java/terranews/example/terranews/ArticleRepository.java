3d01229c01098ff261726578a852cf845a86ef43
mendelson08 <kuba.mendel@wp.pl>
2025-10-07 20:35:59 +0200

    working git

---
+++ src/main/java/terranews/example/terranews/ArticleRepository.java
@@ -0,0 +1,7 @@
+package terranews.example.terranews;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {}

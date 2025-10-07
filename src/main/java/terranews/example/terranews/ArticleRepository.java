3fcd036824ca2287416fb781d752e4ea2a56ed91
mendelson08 <kuba.mendel@wp.pl>
2025-10-07 17:11:25 +0200

    Revert "added entity and repo"
    This reverts commit 959efc3378d018483de31426e7319aefe23fe55a.

--- src/main/java/terranews/example/terranews/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package terranews.example.terranews;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {}
--- src/main/java/terranews/example/terranews/TerranewsApplication.java
+++ src/main/java/terranews/example/terranews/TerranewsApplication.java
@@ -1,48 +1,14 @@
 package terranews.example.terranews;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-
-
-@RestController
 @SpringBootApplication
 public class TerranewsApplication {
-    @Autowired
-    private ArticleRepository articleRepository;
-
-    public static void main(String[] args) {
+static void main(String[] args) {
 		SpringApplication.run(TerranewsApplication.class, args);
 	}
-
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
-    @RequestMapping("articles")
-    public @ResponseBody Iterable<Article> articles() {
-        return articleRepository.findAll();
-    }
-    @RequestMapping("/add")
-    String add() {
-        Article article = new Article();
-
-        // Ustawiamy pola za pomocą setterów
-        article.setTitle("Rząd ogłasza nowy program wsparcia dla studentów");
-        article.setContent("Ministerstwo Edukacji wprowadza nowy program stypendialny mający na celu zwiększenie dostępności edukacji wyższej.");
-        article.setSource("TVN24");
-        article.setDate(LocalDateTime.of(2025, 10, 6, 8, 30, 0));
-
-        articleRepository.save(article);
-        System.out.println("dodano");
-        return "Hello World!";
-    }
 
 }
